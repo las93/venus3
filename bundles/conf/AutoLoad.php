e861384ce59d6ecbc693d7fcb6b6f83be7ec315d
Judicael Paquet <judicael.paquet@batiwiz.com>
2016-09-08 00:01:51 +0200

    change composer - bug fixed

--- bundles/conf/AutoLoad.php
+++ bundles/conf/AutoLoad.php
@@ -43,13 +43,13 @@ spl_autoload_register(function (string $sClassName)
 
         if (strstr($sFileName, 'Venus\\') && file_exists($sFileClassName)) {
 
-        	require __DIR__.$sFileClassName;
+        	require $sFileClassName;
         }
     }
     else {
 
         if (strstr($sFileName, 'Venus\\') && file_exists(preg_replace('#^(src/[a-zA-Z0-9_]+/)#', '$1app/', str_replace(['\\', '/'], '/', str_replace('conf', '', __DIR__).str_replace('Venus\\', '', $sFileName))))) {
-        
+
             require preg_replace('#^(src/[a-zA-Z0-9_]+/)#', '$1app/', str_replace(['\\', '/'], '/', str_replace('conf', '', __DIR__).str_replace('Venus\\', '', $sFileName)));
         }
     }
